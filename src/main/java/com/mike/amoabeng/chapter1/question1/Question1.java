8103871abc3df93bd4ec3da4f728ff94d653eabc
Michael Amoabeng <MichaelAmoabeng@gmail.com>
2015-06-15 12:30:02 -0400

    Update Question1.java
    This is my first crack at question one

--- src/main/java/com/mike/amoabeng/chapter1/question1/Question1.java
+++ src/main/java/com/mike/amoabeng/chapter1/question1/Question1.java
@@ -2,5 +2,26 @@ package com.mike.amoabeng.chapter1.question1;
 
 
 public class Question1 {
+  public static main(String args[]) {
+        
+    /*
+    Implement an algorithm to determine if a string has all unique characters.
+    What if you cannot use additional data structures?
+    */
+        
+        String str = "abcdefg";
 
+        public boolean checkUnique (String str){
+            if (str.length() = 0) System.out.println("No string Entered");
+
+            int check = 0;
+            for (int i = 0; i < str.length(); i++) {
+                int x = str.charAt(i);
+
+                //how do loop throught the string comparing individual characters?
+                if (str.)
+            }
+            return true;
+        }
+    }
 }
