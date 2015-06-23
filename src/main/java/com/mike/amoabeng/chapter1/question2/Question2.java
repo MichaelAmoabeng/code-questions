1acbbdbc6f161dc4c9219703b22cb15a90bf2457
Michael Amoabeng <MichaelAmoabeng@gmail.com>
2015-06-23 12:49:31 -0400

    Update Question2.java

--- src/main/java/com/mike/amoabeng/chapter1/question2/Question2.java
+++ src/main/java/com/mike/amoabeng/chapter1/question2/Question2.java
@@ -2,5 +2,11 @@ package com.mike.amoabeng.chapter1.question2;
 
 
 public class Question2 {
+  
+  /*
+    Implement a function void reverse(char*str) in C or C++ which reverses a null-terminated string.
+     */
+     
+     
 
 }
